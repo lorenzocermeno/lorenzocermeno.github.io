2caf75e43719b72994be640769cb8f00e99801f9
Lorenzo Cermeno <lorenzocermeno@Lorenzos-MacBook-Air.local>
2023-10-29 18:00:23 +0100

    New project update WIP

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -14,6 +14,10 @@ import styled from "@emotion/styled";
 import { FontSize } from "../../constants/constants";
 import Divider from "@mui/material/Divider";
 import { useTheme } from "@mui/material/styles";
+import dockerLogo from "../../images/docker.svg";
+import reactLogo from "../../images/react.svg";
+import springLogo from "../../images/spring.svg";
+import mongoDbLogo from "../../images/mongodb.svg";
 
 interface IProject {
   id: string;
@@ -26,6 +30,12 @@ const SHOE_CONVERTER: IProject = {
   text: "Making it possible to convert the shoe size and gender of one brand to another.",
   title: "Shoe Converter",
 } as const;
+
+const TODO_LIST: IProject = {
+  id: "todo-list",
+  text: "A simple todo list app",
+  title: "Todo List",
+};
 
 const StyledSection = styled.section`
   margin-bottom: 2em;
@@ -112,6 +122,66 @@ export const Projects = (): JSX.Element => {
               href="https://www.npmjs.com/package/shoe-converter"
               id="npm"
               src={npmLogo}
+            />
+          </StyledSpan>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon sx={{ color: "text.primary" }} />}
+          aria-controls={`${TODO_LIST.id}-content`}
+          id={TODO_LIST.id}
+          sx={{ backgroundColor: "background.default" }}
+        >
+          <Grid container spacing={2}>
+            <Grid item xs={8}>
+              <Typography
+                variant="h3"
+                fontSize={FontSize.h3}
+                color="text.primary"
+              >
+                {TODO_LIST.title}
+              </Typography>
+            </Grid>
+            <Grid
+              item
+              xs={4}
+              sx={{ display: "flex", justifyContent: "space-evenly" }}
+            >
+              <StyledImg src={reactLogo} alt="React logo" />
+              <StyledImg src={springLogo} alt="Spring Boot logo" />
+              <StyledImg src={mongoDbLogo} alt="MongoDb logo" />
+              <StyledImg src={dockerLogo} alt="Docker logo" />
+            </Grid>
+          </Grid>
+        </AccordionSummary>
+        <AccordionDetails sx={{ backgroundColor: "background.default" }}>
+          <Typography
+            variant="body1"
+            fontSize={FontSize.p}
+            marginBottom={2}
+            color="text.primary"
+          >
+            {TODO_LIST.text}
+          </Typography>
+          <StyledDiv>
+            <Divider variant="middle" sx={{ borderColor: "#414141" }} />
+          </StyledDiv>
+          <StyledSpan>
+            <Link
+              href="https://github.com/lorenzocermeno/shoe-converter"
+              id="GitHub"
+              src={
+                theme.palette.mode === "dark"
+                  ? githubLogoDarkMode
+                  : githubLogoLightMode
+              }
+            />
+            <Link
+              href="https://www.npmjs.com/package/shoe-converter"
+              id="docker"
+              src={dockerLogo}
             />
           </StyledSpan>
         </AccordionDetails>
