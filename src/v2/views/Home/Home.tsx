8afd24b9b26c41fb0b2267b8ede38c16235eafe1
Lorenzo Cermeno <lorenzocermeno@Lorenzos-MacBook-Air.local>
2024-11-24 21:55:30 +0100

    Added period to home text

--- src/v2/views/Home/Home.tsx
+++ src/v2/views/Home/Home.tsx
@@ -5,7 +5,7 @@ import { COLOR } from "../../constants/constants";
 const firstLine = (fontSize: string) =>
   `<span style="color:${COLOR.SECONDARY};font-size:${fontSize}rem;">Hi!<br/></span>` as const;
 const secondLine = (fontSize: string) =>
-  `<span style="color: ${COLOR.PRIMARY};font-size:${fontSize}rem;">I'm Lorenzo Cermeno, a full stack developer based in Stockholm</span>`;
+  `<span style="color: ${COLOR.PRIMARY};font-size:${fontSize}rem;">I'm Lorenzo Cermeno, a full stack developer based in Stockholm.</span>`;
 
 const StyledDiv = styled.div`
   color: ${COLOR.PRIMARY};
